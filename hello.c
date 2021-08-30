f58d120ca5f3c54537eff36f2087d475fd69b1cc
Ian Brungardt <ibrungardt2@huskers.unl.edu>
2021-08-30 22:07:30 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Ian Brungardt
+ *
+ * Date: 2021/08/30
+ *
+ *Hack number 1git
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ian Brungardt\n");
+  
+
+  return 0;
+}
